6acb1e7d7fba1fc61979759c7781306ac19ebe7b
raysomnath@hotmail.com <raysomnath@hotmail.com>
2018-09-28 08:27:10 +1000

    Modified views.py to start using templates

--- BasicProject/HelloFlask/views.py
+++ BasicProject/HelloFlask/views.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    
+    #html_content = "<html><head><title>Hello Flask</title></head><body>"
+    #html_content += "<strong>Hello Flask!</strong> on " + formatted_now
+    #html_content += "</body></html>"
+
+    #return render_template("index.html", content = "<strong>Hello, Flask!</strong>" + formatted_now);
+
+    return render_template(
+        "index.html",
+        title="Hello Flask",
+        message = "Hello Flask!",
+        content = " on " + formatted_now);
